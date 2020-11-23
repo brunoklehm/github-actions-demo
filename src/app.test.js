e87035ca7076784c06ce62b218f2471260363ade
Bruno Klehm <brunowuklehm@gmail.com>
2020-11-23 18:32:40 -0300

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,7 @@
 const { dayOfTheWeek } = require('./app');
 
 
-test('getDay returns the long-format day of the week', () => {
+test('getDay retorna o formato certo dos dias da semana', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
